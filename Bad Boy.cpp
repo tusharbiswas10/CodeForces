46a6094c3672d8117f3bbcc0c0b4d2426c0eda61
tusharbiswas10 <66631270+tusharbiswas10@users.noreply.github.com>
2021-06-19 11:36:02 +0600

    Create Bad Boy.cpp

---
+++ Bad Boy.cpp
@@ -0,0 +1,31 @@
+    #include <bits/stdc++.h>
+     
+    #define ll          long long int
+    #define endl        "\n"
+     
+     
+     
+     
+    using namespace std;
+     
+    void a() {
+     
+         ll a,b,c,d;
+     
+         cin>>a>>b>>c>>d;
+     
+         cout<<a<<" "<<"1"<<" "<<"1"<<" "<<b<<endl;
+     
+        
+    }
+     
+    int main() {
+      ll test;
+        cin>>test;
+     
+        for (ll i = 0; i < test; ++i)
+        {
+        a();
+      }
+      return 0;
+    } 
