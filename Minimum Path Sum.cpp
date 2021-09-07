375900a454679fcf6c616dfcbca63c75357f8829
tusharbiswas10 <66631270+tusharbiswas10@users.noreply.github.com>
2021-09-07 20:49:56 +0600

    Create Minimum Path Sum.cpp

---
+++ Minimum Path Sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        //  ***** time complexity: O(m*n), approach: DP ******
+        int r=grid.size();
+        int c=grid[0].size();
+        int result=0;
+        /*this will fill only 1st row,
+        so only movement here possible is right */
+        
+        for( int i=0;i<c;i++){
+            result+=grid[0][i];
+            grid[0][i]=result;
+        }
+        
+        /*this will fill only 1st column,
+        so only movement here possible is down */
+        result=0;
+        for( int i=0;i<r;i++){
+            result+=grid[i][0];
+            grid[i][0]=result;
+        }
+        
+        //as 1st row and 1st column is done now we will have to calculate & fill other cells
+        //we will take minimum value from top or left cell and add value from current cell value and store it
+        for(int i=1;i<r;i++)
+		{
+            for(int j=1;j<c;j++)
+            {                                                                            
+                grid[i][j]=min(grid[i-1][j],grid[i][j-1])+grid[i][j];
+            }
+    }
+        // return last cell of grid for result as it stores the min possible path sum
+        return grid[r-1][c-1];
+    }
+};
