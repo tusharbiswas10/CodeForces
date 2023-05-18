036bd89708cb666cee383669dd3dea1d6611e5fa
tusharbiswas10 <66631270+tusharbiswas10@users.noreply.github.com>
2023-05-18 12:45:29 +0600

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include <math.h>
+using namespace std;
+#define ll long long
+
+
+
+// Driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    ll test;//cin>>test;
+    test=1;
+    for(ll iii=0;iii<test;iii++){
+        ll a,b,c;
+        cin>>a;
+        ll t1,t2;
+        if(a%2==0 and a/2>1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+       
+        
+    }
+    return 0;
+}
